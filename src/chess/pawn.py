4f8d7ab951a7fe20d9da739dd8bd021c833701a6
MrTheSteph <samaaron8905@gmail.com>
2024-12-02 15:23:36 +0100

    fixed tuples

--- src/chess/pawn.py
+++ src/chess/pawn.py
@@ -6,19 +6,18 @@ class Pawn(Piece):
         legal_moves = []
         piece_new_position = self.chessboard.get_piece(new_position)
 
-        if self.color == True:
+        if self.color == True: # white
             if piece_new_position == None and (self.position[0], self.position[1] + 1) == new_position:
                 legal_moves.append(self.position,new_position)
             elif piece_new_position.color != self.color:
-                if (self.position[0] - 1, self.position[1] + 1) == piece_new_position.position or self.position + (1,1) == piece_new_position.position:
+                if (self.position[0] - 1, self.position[1] + 1) == piece_new_position.position or (self.position[0] + 1, self.position[1] + 1) == piece_new_position.position:
                     legal_moves.append((self.position,new_position))
 
-        elif self.color == False:
+        elif self.color == False: # black
             if piece_new_position == None and (self.position[0], self.position[1] - 1) == new_position:
-                legal_moves.append(self.position,new_position)
-                # ab hier tuple error beheben
+                legal_moves.append((self.position,new_position))
             elif piece_new_position.color != self.color:
-                if self.position + (-1,-1) == piece_new_position.position or self.position + (1,-1) == piece_new_position.position:
+                if (self.position[0] - 1, self.position[1] - 1) == piece_new_position.position or (self.position[0] + 1, self.position[1] - 1) == piece_new_position.position:
                     legal_moves.append((self.position,new_position))
 
     def is_legal_move(self, new_position):
