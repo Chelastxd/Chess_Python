d20ca660b032132498d8f98e4463b6e766d971c4
MrTheSteph <samaaron8905@gmail.com>
2024-12-11 11:13:51 +0100

    get_paths unfinished doesn't work idk

--- src/chess/piece.py
+++ src/chess/piece.py
@@ -25,3 +25,48 @@ class Piece:
                 self.chessboard.finished = True
         else:
             print("Illegal move!")
+
+    def get_paths(self, diagonal=False, straight=False):
+        legal_moves_list = []
+        if straight:
+            for i in range(0, -self.position[1] + 7): # up
+                if i != 0:
+                    pos = (self.position[0], i)
+                    piece_found = pos in [j.position for j in self.chessboard.piecelist]
+                    if not piece_found:
+                        legal_moves_list.append(pos)
+                    elif piece_found and self.chessboard.get_piece(pos).color != self.color:
+                        legal_moves_list.append(pos)
+                    else:
+                        break
+            for i in range(-self.position[1], 0): # down
+                if i != 0:
+                    pos = (self.position[0], -i)
+                    piece_found = pos in [j.position for j in self.chessboard.piecelist]
+                    if not piece_found:
+                        legal_moves_list.append(pos)
+                    elif piece_found and self.chessboard.get_piece(pos).color != self.color:
+                        legal_moves_list.append(pos)
+                    else:
+                        break
+            for i in range(-self.position[0], 0): # left
+                if i != 0:
+                    pos = (-i, self.position[1])
+                    piece_found = pos in [j.position for j in self.chessboard.piecelist]
+                    if not piece_found:
+                        legal_moves_list.append(pos)
+                    elif piece_found and self.chessboard.get_piece(pos).color != self.color:
+                        legal_moves_list.append(pos)
+                    else:
+                        break
+            for i in range(0, -self.position[0] + 7): # right
+                if i != 0:
+                    pos = (i, self.position[1])
+                    piece_found = pos in [j.position for j in self.chessboard.piecelist]
+                    if not piece_found:
+                        legal_moves_list.append(pos)
+                    elif piece_found and self.chessboard.get_piece(pos).color != self.color:
+                        legal_moves_list.append(pos)
+                    else:
+                        break
+        return legal_moves_list
