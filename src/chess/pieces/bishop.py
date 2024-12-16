b08774bc0e149feba8c80ebe1e1fd33fa397bf43
MrTheSteph <samaaron8905@gmail.com>
2024-12-16 08:42:32 +0100

    bishop and king implementation

--- src/chess/pieces/bishop.py
+++ src/chess/pieces/bishop.py
@@ -2,5 +2,8 @@ from piece import *
 from chessboard import *
 
 class Bishop:
-    def __init__(self):
-        pass
+    def legal_moves(self):
+        return self.get_paths(diagonal=True)
+    
+    def is_legal_move(self, new_position):
+        return new_position in self.legal_moves() # move to pieces in  the future
--- src/chess/pieces/king.py
+++ src/chess/pieces/king.py
@@ -2,5 +2,21 @@ from piece import *
 from chessboard import *
 
 class King:
-    def __init__(self):
-        pass
+    def legal_moves(self):
+        legal_moves_list = []
+        for i in [-1, 0, 1]:
+            for j in [-1, 0, 1]:
+                short_path = [self.position[0]+i, self.position[1]+j]
+
+        for i in self.get_paths(straight=True, diagonal=True):
+            if i in short_path:
+                legal_moves_list.append(i)
+
+        return legal_moves_list
+    
+    def is_legal_move(self, new_position):
+        return new_position in self.legal_moves() # move to pieces in the future
+    
+    def position_is_attacked(position):
+        for piece in self.chessboard.piecelist:
+            pass # check all pieces if they attack here
